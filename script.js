008ce3d3e095b8e92207a8000343ae66fe56b2a7
RAFAEL <rafael.chagas.oliveira@escola.pr.gov.br>
2026-09-18 15:01:31 -0300

    Update Supabase URL and public key
    Updated Supabase URL and key in script.js.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = "COLE_AQUI_SUA_URL";
+const SUPABASE_URL = "https://pkntkbnazykqehilggct.supabase.co/rest/v1/";
 
-const SUPABASE_KEY = "COLE_AQUI_SUA_CHAVE_PUBLICA";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrbnRrYm5henlrcWVoaWxnZ2N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3NDk4OTgsImV4cCI6MjEwNTMyNTg5OH0.K7aDh9ikINZuhL7RFiM91ENgcyARnZDlnvsIBGc8WQA";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
